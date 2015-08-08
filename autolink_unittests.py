68734a6f324e94e7f3babaf0d2808b2938c8f9e9
Issa Rice <riceissa@gmail.com>
2015-08-07 17:44:48 -0700

    add one more test

--- autolink_unittests.py
+++ autolink_unittests.py
@@ -7,6 +7,7 @@ class TestGetFiletypeLink(unittest.TestCase):
         self.assertEqual(al.get_filetype_link("The Biodeterminist's Guide to Parenting", "http://issarice.com", "markdown"), "[The Biodeterminist's Guide to Parenting](http://issarice.com)")
         self.assertEqual(al.get_filetype_link("something.dotcom", "https://google.com/", "markdown"), "[something\.dotcom](https://google.com/)")
         self.assertEqual(al.get_filetype_link("Issa Rice | Issa Rice", "https://google.com/", "markdown"), "[Issa Rice | Issa Rice](https://google.com/)")
+        self.assertEqual(al.get_filetype_link("Issa Rice | Issa Rice", "https://google.com/", "html"), "<a href=\"https://google.com/\">Issa Rice | Issa Rice</a>")
 
     def test_messy_title(self):
         self.assertEqual(al.messy_title_parse("20.1. html — HyperText Markup Language support &mdash; Python 3.4.3 documentation"), "20.1. html — HyperText Markup Language support &mdash; Python 3.4.3 documentation")
